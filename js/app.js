65bdd4c030b6cd8d03c41714b0f2e74b3c59f0f0
Alexandr <yaborodinov@gmail.com>
2020-05-16 13:38:11 +0300

    add form for tickets

--- js/app.js
+++ js/app.js
@@ -1,8 +1,13 @@
-let name = prompt("введите свое имя");
-alert(`hello ${name}`); 
+let name = prompt("Введите свое имя");
+alert(`Добрый день, ${name}!`); 
 
 
 let year = "2020";
-let birthday = prompt(name + ", Хочешь узанть свой возраст?напиши год своего рождения");
-alert(`сеейчас ты узнаешь свой возраст :)`);
-alert(`${name} , тебе ${year- birthday} лет`);
+let birthday = prompt(name + ", Хотели бы узанть свой возраст? Напишите год своего рождения");
+alert(`Сейчас вы узнаете свой возраст ! `);
+alert(`${name} , вам ${year - birthday} лет, не правда ли?`);
+
+
+let cityfrom = prompt(`${name} В каком городе вы сейчас находитесь?`);
+let cityto = prompt("а куда хотели бы полететь?");
+alert(`Авиабилеты из ${cityfrom}  в  ${cityto} сейчас есть в наличии. ${name}, желаете их приобрести?`);
