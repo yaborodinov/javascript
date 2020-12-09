afce98b34a050b69f94e7299848c6feb11010fc7
Alexandr <yaborodinov@gmail.com>
2020-12-09 21:25:58 +0200

    objects exp

--- js/app.js
+++ js/app.js
@@ -1,14 +1,22 @@
 "use strict"
-let name = prompt("Введите свое имя");
-alert(`Добрый день, ${name}!`); 
 
 
-let year = "2020";
-let birthday = prompt(name + ", Хотели бы узнать свой возраст? Напишите год своего рождения");
-alert(`Сейчас Вы узнаете свой возраст! `);
-alert(`${name} , Вам ${year - birthday} лет, не правда ли?`);
+let user = {
+    name: "ivan",
+    age: 34,
+    family: {
+        dad: {
+            name: "Nickolay",
+            age: 50,
+        },
+        mam: "Elena"
+    }
+}
+
+let clone = _.cloneDeep(user);
+
+user["family"]["dad"]["age"]++
 
 
-let cityfrom = prompt(`${name} В каком городе Вы сейчас находитесь?`);
-let cityto = prompt("А куда хотели бы полететь?");
-alert(`Авиабилеты из ${cityfrom}  в  ${cityto} сейчас есть в наличии. ${name}, желаете их приобрести?`);
+console.log(clone)
+console.log(user)
