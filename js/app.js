b0c0a3c5972ed2e729f1282a8f5997a86c76a263
Alexandr <yaborodinov@gmail.com>
2020-12-26 22:28:47 +0200

    добавил возможность лйкать

--- js/app.js
+++ js/app.js
@@ -10,8 +10,19 @@ let buttons = document.querySelectorAll(".add_to_cart");
 for (let i = 0; i < buttons.length; i++){
     
     buttons[i].addEventListener("click", () => {
-        let prevInput = +input.innerHTML;
-         input.innerHTML = `${prevInput+1}`
+        input.innerHTML = `${+input.innerHTML+1}`
         
     })
 }
+
+
+// likes
+
+
+let like = document.querySelectorAll(".wish");
+console.log(like)
+like.forEach((el) => {
+    el.addEventListener("click", () => {
+        el.classList.toggle("wish_like")
+    })
+})
