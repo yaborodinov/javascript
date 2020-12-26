2539353d49f2f6701cede0e5f77b9558389dcef3
Alexandr <yaborodinov@gmail.com>
2020-12-26 21:48:48 +0200

    добавил счетчик в карт

--- js/app.js
+++ js/app.js
@@ -3,35 +3,15 @@
 
 
 
-// let input = document.querySelector(".input");
-// let inputValue = input.getAttribute("value")
-// let button = document.querySelector("button");
+let input = document.querySelector("#products-count");
 
+let buttons = document.querySelectorAll(".add_to_cart");
+
+for (let i = 0; i < buttons.length; i++){
     
-// button.addEventListener("click", function () {
-//     input.setAttribute("value", Number(input.getAttribute("value"))+1 )
-// })
-
-// // picture toggle
-// let a = document.querySelector(".picture1");
-// a.addEventListener("click",  ()=>a.classList.toggle("picture2"))
-
-
-
-
-let a = document.querySelector("img");
-
-
-function changeImg() {
-    let imgSrc = a.getAttribute("src");
-    if (imgSrc === "images/skype.png") {
-        a.setAttribute("src", "images/telegram.png")
-    } else {
-        a.setAttribute("src","images/skype.png")
-    }
+    buttons[i].addEventListener("click", () => {
+        let prevInput = +input.innerHTML;
+         input.innerHTML = `${prevInput+1}`
+        
+    })
 }
-
-
-
-
-a.addEventListener("click", changeImg)
