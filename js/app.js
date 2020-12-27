aef28d68480aac4c82af21ebd93fcd0ed4077016
Alexandr <yaborodinov@gmail.com>
2020-12-27 23:01:40 +0200

    задача поиск ссылок по условию и окрашивание текста в оранжевый. также создал css файлы

--- js/app.js
+++ js/app.js
@@ -2,4 +2,7 @@
 
 
 
-elem2.textContent = "Привет всем"
+let selector = `a[href*="://"]:not([href^="http://internal.com"])`
+let links = document.querySelectorAll(selector);
+
+links.forEach(link => link.style.color = "orange")
