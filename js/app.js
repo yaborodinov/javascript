561dba4f13a3eb4a1f2e9882947469b5a3e25b7f
Alexandr <yaborodinov@gmail.com>
2020-12-27 18:29:46 +0200

    свойство textContent

--- js/app.js
+++ js/app.js
@@ -1,26 +1,5 @@
 "use strict"
 
 
-let salaries = {
-    "john": 100,
-    "pete": 300,
-    "mary":250,
-}
 
-
-function topSalary(salaries) {
-    let max = 0;
-    let maxName = null;
-    let arr = Object.entries(salaries)
-
-    for (let [name, salary] of arr) {
-        if (max < salary) {
-            max = salary
-            maxName = name;
-        }
-    }
-    return maxName
-}
-
-
-console.log(topSalary(salaries))
+elem2.textContent = "Привет всем"
