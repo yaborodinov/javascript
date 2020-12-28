51b8cffc53a770481998f8fde63588b393fea5d6
Alexandr <yaborodinov@gmail.com>
2020-12-28 18:04:26 +0200

    изменение документа

--- js/app.js
+++ js/app.js
@@ -2,7 +2,33 @@
 
 
 
-let selector = `a[href*="://"]:not([href^="http://internal.com"])`
-let links = document.querySelectorAll(selector);
+let div = document.createElement("div")
+div.className="alert";
+div.innerHTML = "<strong>Всем привет!!!</strong> Вы прочитали не менее важное сообщение"
 
-links.forEach(link => link.style.color = "orange")
+let main = document.querySelector("main");
+main.append(div)
+
+let a = document.createElement("h1");
+a.className = "alert"
+a.textContent="hello niggers"
+main.prepend(a)
+setTimeout(() => a.remove(), 2000)
+
+main.append(a.cloneNode(true))
+
+// DocumentFragment
+
+function getListContent() {
+    let fragment = new DocumentFragment();
+    for (let i = 1; i <= 3; i++){
+        let li = document.createElement("li");
+        li.append(i);
+        fragment.append(li)
+    }
+    return fragment;
+}
+
+
+ul.append(getListContent())
+
