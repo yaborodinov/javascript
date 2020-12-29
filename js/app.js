4436a232b4022d79c7bf58a3445507b6620fc06e
Alexandr <yaborodinov@gmail.com>
2020-12-29 18:09:46 +0200

    добавил модальное окно

--- js/app.js
+++ js/app.js
@@ -20,9 +20,41 @@ for (let i = 0; i < buttons.length; i++){
 
 
 let like = document.querySelectorAll(".wish");
-console.log(like)
 like.forEach((el) => {
     el.addEventListener("click", () => {
         el.classList.toggle("wish_like")
     })
 })
+
+
+
+
+// modal window
+
+let moreDetailsButtons = document.querySelectorAll(".more_details");
+let closeButton = document.querySelector(".btn-close");
+let modal = document.querySelector(".main_modal");
+console.log(closeButton)
+
+moreDetailsButtons.forEach((el) =>{
+    el.addEventListener("click", () => {
+        modal.classList.remove("hide")
+        modal.classList.add("show")
+
+    })
+})
+
+function closeModal() {
+    modal.classList.toggle("show")
+}
+
+closeButton.addEventListener("click", closeModal)
+
+
+modal.addEventListener("click", (e) => {
+    if (e.target === modal) {
+        closeModal()
+    }
+})
+
+
