f06ec9f561c8c86b5e91f5169bee74109f938bc0
Alexandr <yaborodinov@gmail.com>
2021-01-02 17:05:36 +0200

    перемещение мяча п ополю с помощью js

--- js/app.js
+++ js/app.js
@@ -1,34 +1,22 @@
 "use strict"
 
 
-
-let div = document.createElement("div")
-div.className="alert";
-div.innerHTML = "<strong>Всем привет!!!</strong> Вы прочитали не менее важное сообщение"
-
-let main = document.querySelector("main");
-main.append(div)
-
-let a = document.createElement("h1");
-a.className = "alert"
-a.textContent="hello niggers"
-main.prepend(a)
-setTimeout(() => a.remove(), 2000)
-
-main.append(a.cloneNode(true))
-
-// DocumentFragment
-
-function getListContent() {
-    let fragment = new DocumentFragment();
-    for (let i = 1; i <= 3; i++){
-        let li = document.createElement("li");
-        li.append(i);
-        fragment.append(li)
-    }
-    return fragment;
-}
+let fieldHeight = field.clientHeight;
+let fieldWidth = field.clientWidth;
 
 
-ul.append(getListContent())
+let ballWidth = ball.clientWidth;
+let ballHeight = ball.clientHeight;
+
+ball.style.cssText = `left:${fieldWidth / 2 - ballWidth/2}px; top:${fieldHeight / 2 - ballHeight/2}px`
+
+console.log(fieldHeight)
+
+
+
+
+
+
+
+
 
