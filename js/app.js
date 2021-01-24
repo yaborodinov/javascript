f8610fb02fd0d94fedd00e3355925cef4169d6de
Alexandr <yaborodinov@gmail.com>
2021-01-24 20:49:33 +0200

    задача Создайте список, в котором элементы могут быть выделены, как в файловых менеджерах.

--- js/app.js
+++ js/app.js
@@ -1,17 +1,35 @@
 "use strict"
 
-let thumbs = document.querySelector("#thumbs");
 
-thumbs.addEventListener("click", (e) => {
-    let thumbnail = e.target.closest("a");
 
-    if (!thumbnail) return;
 
-    showThumbnail(thumbnail.href, thumbnail.title)
-    e.preventDefault();
+
+
+
+let ul = document.querySelector("ul");
+
+ul.addEventListener("click", e => {
+    if (e.target.tagName != "LI") return;
+
+    if (e.ctrlKey || e.metaKey) {
+        toggleSelect(e.target)
+    } else {
+        singleSelect(e.target)
+    }
 })
 
-function showThumbnail(href, title) {
-    largeImg.src = href;
-    largeImg.alt = title;
+ul.addEventListener("mousedown", e => {
+    e.preventDefault()
+})
+
+function toggleSelect(element) {
+    element.classList.toggle("active")
+}
+
+
+function singleSelect(element) {
+    let selected = document.querySelectorAll(".active");
+    for (let element of selected) {
+        element.classList.remove("active")
+    }element.classList.add("active")
 }
