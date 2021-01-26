9136ee41913d46d3db839d5d43f95752965416ea
Alexandr <yaborodinov@gmail.com>
2021-01-26 16:04:29 +0200

    использование метода GET как поиск в Google

--- js/app.js
+++ js/app.js
@@ -1,35 +1,16 @@
 "use strict"
 
 
+let form = document.createElement("form");
+form.action = "https://google.com/search";
+form.method = "GET"
+
+form.innerHTML = `<input name="q" value="test">`;
+document.body.append(form)
 
 
+let btn = document.querySelector("button")
 
-
-
-let ul = document.querySelector("ul");
-
-ul.addEventListener("click", e => {
-    if (e.target.tagName != "LI") return;
-
-    if (e.ctrlKey || e.metaKey) {
-        toggleSelect(e.target)
-    } else {
-        singleSelect(e.target)
-    }
+btn.addEventListener("click", () => {
+    form.submit()
 })
-
-ul.addEventListener("mousedown", e => {
-    e.preventDefault()
-})
-
-function toggleSelect(element) {
-    element.classList.toggle("active")
-}
-
-
-function singleSelect(element) {
-    let selected = document.querySelectorAll(".active");
-    for (let element of selected) {
-        element.classList.remove("active")
-    }element.classList.add("active")
-}
