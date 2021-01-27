80984e4da8e8a6a025d32daa16e93a310a2ea306
Alexandr <yaborodinov@gmail.com>
2021-01-27 15:18:58 +0200

    метод GET через Fetch

--- js/app.js
+++ js/app.js
@@ -6,38 +6,16 @@
 
 let requestURL = "https://jsonplaceholder.typicode.com/users";
 
-function sendRequest(method, url, body = null) {
-    return new Promise((resolve, reject) => {
-        
-
-    const xhr = new XMLHttpRequest();
-        xhr.responseType = "json";
-        
-
-
-        xhr.open(method, url);
-        xhr.setRequestHeader("Content-Type", "application/json")
-
-        
-    xhr.addEventListener("load", () => {
-        if (xhr.status >= 400) {
-            resolve(xhr.response)
-        } else {
-            reject(xhr.response)
-        }
-    })
-        xhr.onerror = () => {
-            reject(xhr.response)
-        }
-
-    xhr.send(JSON.stringify(body)); 
-    })
+function sendRequest(method, url) {
+    return fetch(url).then(response => {
+       return response.json()
+   })
    
 }
 
-// sendRequest("GET", requestURL)
-//     .then(data => console.log(data))
-//     .catch(error=>console.log(error))
+sendRequest("GET", requestURL)
+    .then(data => console.log(data))
+    .catch(error=>console.log(error))
 
 let body = {
     name: "Andrey",
@@ -45,6 +23,6 @@ let body = {
 }
 
 
-sendRequest("POST", requestURL, body)
-    .then(data => console.log(data))
-    .catch(error=>console.log(error))
+// sendRequest("POST", requestURL, body)
+//     .then(data => console.log(data))
+//     .catch(error=>console.log(error))
