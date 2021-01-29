baad67bbff414eb7445f4effe98d3374f22a3b99
Alexandr <yaborodinov@gmail.com>
2021-01-29 22:51:08 +0200

    функция sleep. показывает как просто работать с promise

--- js/app.js
+++ js/app.js
@@ -21,36 +21,48 @@ console.log("request Data ...")
 
 
 
-const p = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        console.log("preparing Data....");
-        const backendData = {
-        server: "aws",
-        port: 2000,
-        status:"working",
-        }
-        resolve(backendData)
+// const p = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         console.log("preparing Data....");
+//         const backendData = {
+//         server: "aws",
+//         port: 2000,
+//         status:"working",
+//         }
+//         resolve(backendData)
 
-    },2000)
-}).then((data)=> {
- return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            data.modifaied = true;
-            resolve(data)
-        },2000)
-    })   
+//     },2000)
+// }).then((data)=> {
+//  return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             data.modifaied = true;
+//             resolve(data)
+//         },2000)
+//     })   
     
     
-})
+// })
     
-    .then((clientData) => {
+//     .then((clientData) => {
     
-    console.log("data received", clientData)
-    clientData.momsName = "Vasilisa";
-    return clientData
-}).then((someData) => {
-    console.log("modified", someData)
-}).catch(err => console.error("errorrr: ", err))
-    .finally(() => {
-    console.log("finally!!!")
-})
+//     console.log("data received", clientData)
+//     clientData.momsName = "Vasilisa";
+//     return clientData
+// }).then((someData) => {
+//     console.log("modified", someData)
+// }).catch(err => console.error("errorrr: ", err))
+//     .finally(() => {
+//     console.log("finally!!!")
+// })
+
+const sleep = (ms) => {
+    return new Promise((resolve)=> {
+    setTimeout(() => {
+        resolve()
+    }, ms)
+    })
+
+}
+
+sleep(2000).then(() => console.log("after 2 sec"))
+sleep(4000).then(()=>console.log("after 4 sec"))
