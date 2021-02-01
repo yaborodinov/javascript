9ee4202aab53a40016efd3d6e5169a156c93bb43
Alexandr <yaborodinov@gmail.com>
2021-02-01 14:15:03 +0200

    задача Создайте интерфейс, позволяющий ввести сумму банковского вклада и процент, а затем рассчитать, какая это будет сумма через заданный промежуток времени.

--- js/app.js
+++ js/app.js
@@ -3,76 +3,28 @@
 
 
 
-console.log("request Data ...")
+let form = document.forms.calculator
 
-// setTimeout(() => {
-//     console.log("preparing Data...");
-//     const backendData = {
-//         server: "aws",
-//         port: 2000,
-//         status:"working",
-//     }
+form.money.oninput = calculate;
+form.months.onchange = calculate;
+form.interest.oninput = calculate;
 
-//     setTimeout(() => {
-//         backendData.modifaied = true;
-//         console.log("Data resived", backendData)
-//     },2000)    
-// }, 2000)
+function calculate() {
+    let initial = +form.money.value;
+    if (!initial) return;
+    let interest = form.interest.value * 0.01;
+    if (!interest) return;
 
+    let years = form.months.value / 12;
+    if (!years) return;
 
+    let result = Math.round(initial * (1 + interest * years));
 
-// const p = new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//         console.log("preparing Data....");
-//         const backendData = {
-//         server: "aws",
-//         port: 2000,
-//         status:"working",
-//         }
-//         resolve(backendData)
-
-//     },2000)
-// }).then((data)=> {
-//  return new Promise((resolve, reject) => {
-//         setTimeout(() => {
-//             data.modifaied = true;
-//             resolve(data)
-//         },2000)
-//     })   
-    
-    
-// })
-    
-//     .then((clientData) => {
-    
-//     console.log("data received", clientData)
-//     clientData.momsName = "Vasilisa";
-//     return clientData
-// }).then((someData) => {
-//     console.log("modified", someData)
-// }).catch(err => console.error("errorrr: ", err))
-//     .finally(() => {
-//     console.log("finally!!!")
-// })
-
-const sleep = (ms) => {
-    return new Promise((resolve)=> {
-    setTimeout(() => {
-        resolve()
-    }, ms)
-    })
+    let height = result / form.money.value * 100 + "px";
+    document.getElementById("height-after").style.height = height;
+    document.getElementById("money-before").innerHTML = form.money.value;
+    document.getElementById("money-after").innerHTML = result;
 
 }
 
-// sleep(2000).then(() => console.log("after 2 sec"))
-// sleep(4000).then(()=>console.log("after 4 sec"))
-
-// метод all()
-Promise.all([sleep(2000), sleep(5000)])
-    .then(() => {
-    console.log("All prises")
-    })
-
-    // метод rece()
-Promise.race([sleep(2000), sleep(5000)]).then(() => console.log("Race promises"))
-    
+calculate()
