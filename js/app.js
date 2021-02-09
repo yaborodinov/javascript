bc271647e1f084c7193c39a5cfbce3fc57e7320a
Alexandr <yaborodinov@gmail.com>
2021-02-09 18:44:39 +0200

    объекты (геттерыб сеттеры)

--- js/app.js
+++ js/app.js
@@ -2,8 +2,41 @@
 
 
 
-let form = document.forms.searcher;
-console.log(form);
 
-form.method = "GET";
-form.action = "https://google.com/search";
+const person = Object.create({
+    calculateAge() {
+        console.log("Age ", new Date().getFullYear() - this.birthYear )
+    }
+}, {
+    name: {
+        value: "Ivan",
+        enumerable: true,
+        configurable: true,
+    },
+    birthYear:{
+        value: 1990,
+        enumerable: true,
+        writable:true,
+    },
+    age: {
+        get() {
+            return new Date().getFullYear() - this.birthYear
+        },
+        set(value) {
+            document.body.style.background = "green"
+            console.log("set age", value)
+        }
+    }
+})
+
+
+
+
+for (let key in person) {
+    if (
+        person.hasOwnProperty(key)
+    ) {
+        console.log("KEY ", key)
+    }
+    
+}
