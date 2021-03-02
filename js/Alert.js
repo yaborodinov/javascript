e1b5eb1044997c59d1030db25ec7cbfc1bab50c8
Alexandr <yaborodinov@gmail.com>
2021-03-02 16:19:08 +0200

    практический js. Работа с классами(основная структура), наследование классов

---
+++ js/Alert.js
@@ -0,0 +1,10 @@
+class Alert {
+    constructor(a, c, d) {
+        this.message = a;
+        this.cssClass = c;
+        this.out = d;
+    }
+    showAlert() {
+        document.querySelector(this.out).innerHTML = `<p class="${this.cssClass}">${this.message}</p>`
+    }
+}
--- js/app.js
+++ js/app.js
@@ -1,77 +1,31 @@
 "use strict"
-// function Clock({ template }) {
 
-//     let timer;
+function myAlert(a, c, d) {
 
-//     function render() {
-//         let date = new Date();
-
-//         let hours = date.getHours();
-//         if (hours < 10) hours = '0' + hours;
-
-//         let mins = date.getMinutes();
-//         if (mins < 10) mins = '0' + mins;
-
-//         let secs = date.getSeconds();
-//         if (secs < 10) secs = '0' + secs;
-
-//         let output = template
-//             .replace('h', hours)
-//             .replace('m', mins)
-//             .replace('s', secs);
-
-//         console.log(output);
-//     }
-
-//     this.stop = function () {
-//         clearInterval(timer);
-//     };
-
-//     this.start = function () {
-//         render();
-//         timer = setInterval(render, 1000);
-//     };
-
-// }
-
-// let clock = new Clock({ template: 'h:m:s' });
-// clock.start();
-
-
-class Clock{
-
-    constructor({template}) {
-        this.template = template;
-    }
-
-    render() {
-        let date = new Date();
-
-        let hours = date.getHours();
-        if (hours < 10) hours = "0" + hours;
-
-        let min = date.getMinutes();
-        if (min < 10) min = "0" + min;
-
-        let secs = date.getSeconds();
-        if (secs < 10) secs = "0" + secs;
-
-        let output = this.template
-            .replace("h", hours)
-            .replace("m", min)
-            .replace("s", secs)
-        console.log(output)
-    }
-        stop(){
-            clearInterval(this.timer)
-        }
-
-        start(){
-            this.render();
-            this.timer = setInterval(()=>this.render(),1000)
-        }
-    
+    let b = `<p class="${c}">${a}</p>`;
+    document.querySelector(d).innerHTML = b
 }
 
-let clock = new Clock({ template: "h:m:s" })
-clock.start()
+
+myAlert("hi", "red", ".test");
+myAlert("Goodbay", "orange", ".test2")
+
+
+
+
+
+
+
+
+
+let m = new Alert("my message", "orange", ".test");
+
+m.showAlert()
+
+let m2 = new Alert2("my message", "orange", ".test", "check_circle_outline");
+m2.showIconAlert()
+
+
+
+let m3 = new Alert2("приветствую", "red", ".icon", "alarm_on");
+m3.showIconAlert()
