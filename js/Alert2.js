5db5ffb297584bdd2d991e68d581dda8bd4f6625
Alexandr <yaborodinov@gmail.com>
2021-03-08 17:25:11 +0200

    простой и понятный код, реализующий свайп

--- js/Alert2.js
+++
@@ -1,9 +0,0 @@
-class Alert2 extends Alert {
-    constructor(a, c, d, icon) {
-        super(a, c, d);
-        this.icon = icon;
-    }
-    showIconAlert() {
-        document.querySelector(this.out).innerHTML = `<p class="${this.cssClass}">${this.message}<span class="material-icons">${this.icon}</span></p>`
-    }
-}
--- js/app.js
+++ js/app.js
@@ -1,31 +1,44 @@
 "use strict"
 
-function myAlert(a, c, d) {
 
-    let b = `<p class="${c}">${a}</p>`;
-    document.querySelector(d).innerHTML = b
+
+document.addEventListener("touchstart", handleTouchStart, false)
+document.addEventListener("touchmove", handleTouchMove, false)
+
+const logBlock = document.querySelector(".log-block");
+
+let x1 = null;
+let y1 = null;
+
+
+function handleTouchStart(e) {
+    let firstTouch = e.touches[0];
+    x1 = firstTouch.clientX;
+    y1 = firstTouch.clientY;
+    console.log(x1,y1)
 }
 
 
-myAlert("hi", "red", ".test");
-myAlert("Goodbay", "orange", ".test2")
+function handleTouchMove(e) {
+    if (!x1 || !y1) {
+        return false;
+    }
+    let x2 = e.touches[0].clientX;
+    let y2 = e.touches[0].clientY;
+    // console.log(x2, y2);
+
+    let xDiff = x2 - x1;
+    let yDiff = y2 - y1;
 
 
-
-
-
-
-
-
-
-let m = new Alert("my message", "orange", ".test");
-
-m.showAlert()
-
-let m2 = new Alert2("my message", "orange", ".test", "check_circle_outline");
-m2.showIconAlert()
-
-
-
-let m3 = new Alert2("приветствую", "red", ".icon", "alarm_on");
-m3.showIconAlert()
+    if (Math.abs(xDiff) > Math.abs(yDiff)) {
+        if (xDiff > 0) console.log("right")
+        else console.log("left")
+    }
+    else {
+        if (yDiff > 0) console.log("down")
+        else console.log("top")
+    }
+    x1 = null;
+    x2 = null;
+}
